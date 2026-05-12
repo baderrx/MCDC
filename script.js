9c376a416a93d988a2870d0f4f42c074c99e58e0
baderrx <bader.cmm@hotmail.com>
2026-05-12 20:08:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ const companyLink = "https://maps.app.goo.gl/QzhkZYZS1Q8LveV97";
 
 // ========== صور الفنادق (34 صورة) ==========
 const hotelImages = [
-    "img/1.jpeg","img/2.jpg","img/3.jpg","img/4.jpg","img/5.jpg","img/6.jpg","img/7.jpg","img/img/8.1.jpg","img/9.png","img/10.jpg","img/11.jpg","img/12.jpg","img/13.jpg","img/14.png","img/15.jpg","img/16.jpg",
+    "img/1.jpeg","img/2.jpg","img/3.jpg","img/4.jpg","img/5.jpg","img/6.jpg","img/7.jpg","img/img/8.1.jpeg","img/9.png","img/10.jpg","img/11.jpg","img/12.jpg","img/13.jpg","img/14.png","img/15.jpg","img/16.jpg",
     "img/17.jpg","img/18.jpg","img/19.jpg","img/20.jpg","img/21.jpg","img/22.jpg","img/23.jpg","img/24.jpg","img/25.jpg","img/26.jpeg","img/27.jpg","img/28.jpg","img/29.jpg","img/30.jpg","img/31.png",
     "img/32.jpg","img/33.jpg","img/34.jpg"
 ];
